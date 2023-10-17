382241d4a1ce9ecdb1a2b4b62dd1e2d915977928
Silibop <112124515+Silibop@users.noreply.github.com>
2023-10-17 11:43:29 -0400

    run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# This assumes your program is named index.js
+find . -name . -o -prune -exec rm -rf -- {} +
+git clone https://github.com/Silibop/director_docs ./
+npm install
 exec bin/www
